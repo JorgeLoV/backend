9fdef76dfb7ffb6dedd46d9e83f8768a12cf145d
JorgeLoV <150234992+JorgeLoV@users.noreply.github.com>
2024-05-16 11:13:31 +0200

    Delete databse directory

--- databse/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost:27017/rest_api')
-    .then(() => console.log('Conectado a la base de datos'))
-    .catch((error) => console.error(error))
-    
